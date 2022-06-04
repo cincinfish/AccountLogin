23de6b2412659787879f82d60afbeba250b62a58
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-06-05 00:15:14 +0800

    feat: add login route

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 const exphbs = require('express-handlebars')
 
-app.use(methodOverride('_method'))
-app.use(routes)
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(routes)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
